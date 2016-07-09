6c5f008234f357a611fa32988eb751dd4aea0064
talhaobject90 <talhaqqq@gmail.com>
2016-07-09 10:07:28 +0530

    after ember-calendar working

--- app/controllers/dashboard/adminleave.js
+++ app/controllers/dashboard/adminleave.js
@@ -2,4 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   inputFormat:'DD/MM/YYYY',
+
+  occurrences: Ember.A(),
+
+  actions: {
+    calendarAddOccurrence: function(occurrence) {
+      this.get('occurrences').pushObject(Ember.Object.create({
+        title: occurrence.get('title'),
+        startsAt: occurrence.get('startsAt'),
+        endsAt: occurrence.get('endsAt')
+      }));
+    },
+
+    calendarUpdateOccurrence: function(occurrence, properties) {
+      occurrence.setProperties(properties);
+    },
+
+    calendarRemoveOccurrence: function(occurrence) {
+      this.get('occurrences').removeObject(occurrence);
+    }
+  }
 });

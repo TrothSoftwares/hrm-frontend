ba1cce8381b6fdb94c4f4a9323cb8e54d0243781
talhaobject90 <talhaqqq@gmail.com>
2016-10-18 17:50:26 +0530

    after fixing anaylictcs graph color

--- app/controllers/dashboard/analytics.js
+++ app/controllers/dashboard/analytics.js
@@ -5,26 +5,21 @@ export default Ember.Controller.extend({
 
 
 
-  salaryData:Ember.computed('employees',function(){
-     return {
-
-
-
-
-
-       labels: this.get('employees').mapBy('firstname'),
-       datasets: [{
-         label: "Salary",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-         data: this.get('employees').mapBy('gross')
-       }]
-     };
-   }),
+  // salaryData:Ember.computed('employees',function(){
+  //    return {
+  //      labels: this.get('employees').mapBy('firstname'),
+  //      datasets: [{
+  //        label: "Salary",
+  //         fillColor: "rgba(220,220,220,0.2)",
+  //         strokeColor: "rgba(220,220,220,1)",
+  //         pointColor: "rgba(220,220,220,1)",
+  //         pointStrokeColor: "#fff",
+  //         pointHighlightFill: "#fff",
+  //         pointHighlightStroke: "rgba(220,220,220,1)",
+  //        data: this.get('employees').mapBy('gross')
+  //      }]
+  //    };
+  //  }),
 
 
 
--- app/routes/dashboard/analytics.js
+++ app/routes/dashboard/analytics.js
@@ -44,6 +44,15 @@ export default Ember.Route.extend({
 
 
 
+    var salaryArray=[];
+    model.employees.forEach(function(empl){
+      salaryArray.push(empl.get('gross'));
+    });
+
+
+
+
+
     controller.set('attendanceData',{
     labels: [
         "0-25 %",
@@ -68,6 +77,38 @@ export default Ember.Route.extend({
                 "#FFCE56"
             ]
         }]
+});
+
+
+
+controller.set('salaryData',{
+  labels: model.employees.mapBy('firstname'),
+  datasets: [{
+    label: "Salary",
+    backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+     fillColor: "rgba(220,220,220,0.2)",
+     strokeColor: "rgba(220,220,220,1)",
+     pointColor: "rgba(220,220,220,1)",
+     pointStrokeColor: "#fff",
+     pointHighlightFill: "#fff",
+     pointHighlightStroke: "rgba(220,220,220,1)",
+    data: model.employees.mapBy('gross')
+  }]
 });
 
 

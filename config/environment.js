7e4c262777300cb317fd67d8cdc1757e18c6434f
talhaqqq <talhaqqq@gmail.com>
2016-10-19 15:00:33 +0530

    after setting back url to trothhr -back url

--- config/environment.js
+++ config/environment.js
@@ -71,7 +71,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.host =  'http://trothhr-backend.herokuapp.com';
+    ENV.APP.host =  'http://trothhr-back.herokuapp.com';
   }
 
   return ENV;

619a46edaf6f2e5c07c82a4ab014ba48b7efb4ee
talhaqqq <talhaqqq@gmail.com>
2016-10-31 16:26:55 +0530

    after upgrading to all good versions

--- app/components/initialize-semanticuitabs.js
+++ app/components/initialize-semanticuitabs.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
+
+
+
 export default Ember.Component.extend({
    didInsertElement: function(){
-  Ember.$('.menu .item').tab();
-
-  Ember.$('.ui.accordion').accordion();
-  Ember.$('.small.modal').modal();
+  $('.menu .item').tab();
+  $('.ui.accordion').accordion();
+  $('.small.modal').modal();
  }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,8 +6,10 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
 
+ 
 
-  
+
+
   });
 
   // Use `app.import` to add additional libraries to the generated
